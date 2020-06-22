e5ba9cf009bc04cd2f3b37df1224f2c9a43bfb0b
jeevantk <jeevan.thomaskoshy@gmail.com>
2020-06-22 09:53:30 +0530

    updated path

--- create.js
+++ create.js
@@ -2,8 +2,6 @@ import * as uuid from "uuid";
 import handler from "./libs/handler-lib";
 import dynamoDb from "./libs/dynamodb-lib";
 
-const dynamoDb = new AWS.DynamoDB.DocumentClient();
-
 export const main =  handler(async (event, context) => {
     const data = JSON.parse(event.body);
     const params = {
